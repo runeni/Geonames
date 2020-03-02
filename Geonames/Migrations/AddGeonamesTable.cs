c948d8dbe8a37433885b438d097169a412f36227
Rune Mydske Nielsen <rune.nielsen@knowit.no>
2020-03-02 14:06:48 +0100

    Add usage of fluent migrator
    This is only a POC. Will be replaced with real migrations.

---
+++ Geonames/Migrations/AddGeonamesTable.cs
@@ -0,0 +1,23 @@
+﻿using FluentMigrator;
+using FluentMigrator.Example.Migrations;
+
+namespace Geonames.Migrations
+{
+    [Migration(20200302134800)]
+    public class AddGeonamesTable : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("Notes")
+                .WithIdColumn()
+                .WithColumn("Body").AsString(4000).NotNullable()
+                .WithTimeStamps()
+                .WithColumn("User_id").AsInt32();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("Notes");
+        }
+    }
+}
