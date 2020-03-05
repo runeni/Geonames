a89f8e33439f6f390e2d58de1a2fe580dc740f6b
Rune Mydske Nielsen <rune.nielsen@knowit.no>
2020-03-05 15:44:06 +0100

    Add featureclassifications and fill with data

---
+++ Geonames/Migrations/20200305150700_AddTableFeatureClassifications.cs
@@ -0,0 +1,24 @@
+﻿using FluentMigrator;
+using Geonames.Extensions;
+
+namespace Geonames.Migrations
+{
+    [Migration(20200305150700)]
+    public class AddTableFeatureClassifications : Migration
+    {
+        public override void Up()
+        {
+            Create.Table("featureclassifications")
+                .WithColumn("id").AsInt32().PrimaryKey().Identity()
+                .WithColumn("class_code").AsString(12).NotNullable().Indexed()
+                .WithColumn("content").AsString(64).NotNullable()
+                .WithColumn("description").AsString(255).Nullable()
+                .WithTimeStamps();
+        }
+
+        public override void Down()
+        {
+            Delete.Table("featureclassifications");
+        }
+    }
+}
