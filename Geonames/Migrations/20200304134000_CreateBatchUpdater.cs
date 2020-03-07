9f4f81b4f7c08300a11871bffeb2a83db2de42c6
Rune Mydske Nielsen <rune.mydske.nielsen@gyldendal.no>
2020-03-07 17:46:08 +0100

    Split up in 2 migrations

---
+++ Geonames/Migrations/20200304134000_CreateBatchUpdater.cs
@@ -0,0 +1,47 @@
+using FluentMigrator;
+
+namespace Geonames.Migrations
+{
+    [Migration(20200304134000)]
+    public class CreateBatchUpdater : BaseMigration
+    {
+        public override void Up()
+        {
+            Execute.WithConnection(async (conn, tran) =>
+            {
+                var cmd = @"CREATE OR REPLACE FUNCTION batch_update_name_tsv()
+                          RETURNS void AS
+                        $BODY$
+                        DECLARE
+
+                        id_val integer;
+
+                        BEGIN
+
+                          FOR id_val IN 0..120
+                          LOOP
+
+                          UPDATE geonames SET name_tsv = to_tsvector(name)
+                            WHERE id > (100000 * id_val) AND id <= 100000 * (id_val + 1);
+
+                          END LOOP;
+
+                        END
+                        $BODY$
+                        LANGUAGE plpgsql;";
+                var rowsAffected = await ExecuteSqlAsync(conn, tran, cmd);
+            });
+
+        }
+
+        public override void Down()
+        {
+            Execute.WithConnection(async (conn, tran) =>
+            {
+                var cmd = "DROP FUNCTION batch_update_name_tsv()";
+                var rowsAffected = await ExecuteSqlAsync(conn, tran, cmd);
+            });
+        }
+        
+    }
+}
--- Geonames/Migrations/20200304134500_SetTsVectors.cs
+++ Geonames/Migrations/20200304134500_SetTsVectors.cs
@@ -1,4 +1,5 @@
-﻿using Dapper;
+﻿using System;
+using Dapper;
 using FluentMigrator;
 using Microsoft.AspNetCore.Mvc.ApiExplorer;
 using Npgsql;
@@ -12,35 +13,10 @@ namespace Geonames.Migrations
         {
             Execute.WithConnection(async (conn, tran) =>
             {
-                var cmd = @"CREATE OR REPLACE FUNCTION batch_update_name_tsv()
-                          RETURNS void AS
-                        $BODY$
-                        DECLARE
-
-                        id_val integer;
-
-                        BEGIN
-
-                          FOR id_val IN 0..120
-                          LOOP
-
-                          UPDATE geonames SET name_tsv = to_tsvector(name)
-                            WHERE id > (100000 * id_val) AND id <= 100000 * (id_val + 1);
-
-                          END LOOP;
-
-                        END
-                        $BODY$
-                        LANGUAGE plpgsql;";
+                var cmd = "select batch_update_name_tsv();";
                 var rowsAffected = await ExecuteSqlAsync(conn, tran, cmd);
             });
 
-            // Use this approach to get around timeout issues.
-            Execute.WithConnection(async (conn, tran) =>
-            {
-                var cmd = "select batch_update_name_tsv()";
-                var rowsAffected = await ExecuteSqlAsync(conn, tran, cmd);
-            });
         }
 
         public override void Down()
@@ -49,12 +25,6 @@ namespace Geonames.Migrations
             Execute.WithConnection(async (conn, tran) =>
             {
                 var cmd = "UPDATE geonames SET name_tsv = null";
-                var rowsAffected = await ExecuteSqlAsync(conn, tran, cmd);
-            });
-
-            Execute.WithConnection(async (conn, tran) =>
-            {
-                var cmd = "DROP FUNCTION batch_update_name_tsv()";
                 var rowsAffected = await ExecuteSqlAsync(conn, tran, cmd);
             });
         }
